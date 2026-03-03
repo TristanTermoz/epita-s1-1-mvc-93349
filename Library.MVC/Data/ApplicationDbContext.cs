76f398c90de0786179a8436d572f7d5c956e683f
Tristan <tristan.termoz@epita.fr>
2026-03-03 17:17:20 +0000

    everything is working

--- Library.MVC/Data/ApplicationDbContext.cs
+++ Library.MVC/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using Library.Domain;
 namespace Library.MVC.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Product> Products { get; set; }
     }
 }
